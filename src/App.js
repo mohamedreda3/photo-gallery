8da8e64640ed811ac6c62ce0cfbd26c7faaff8b2
mohamedreda3 <mmoh33653@gmail.com>
2023-04-16 15:23:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getPhotoes } from "./store/slices/photoesSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { FaWindowClose } from "react-icons/fa";
 
 function App() {
-  const [phs, setPhs] = useState([]);
+  const [photoView, setPhotoView] = useState(false);
+  const [photoViewSrc, setPhotoViewSrc] = useState(null);
+
   const dispatch = useDispatch();
   const photoes = useSelector((state) => state.photo);
   useEffect(() => {
     dispatch(getPhotoes());
-    // setPhs([...photoes]);
   }, [dispatch]);
   return (
     <div className="App">
-      <h1>PHOTO GALLERY</h1>
-      <p>This a photo gallery using redux-toolkit and redux-thunk</p>
-      
+      <nav>
+        <h1>PHOTO GALLERY</h1>
+        <p>This a photo gallery using redux-toolkit and redux-thunk</p>
+      </nav>
       {photoes.status == "loading" ? <div className="loader"></div> : null}
-      
+
       <div className="image-container">
         {photoes.data.length > 0
           ? photoes.data.map((item, index) => {
@@ -27,11 +30,23 @@ function App() {
                   src={item.download_url}
                   alt={item.author}
                   key={index}
+                  onClick={() => {
+                    setPhotoView(true);
+                    setPhotoViewSrc(item.download_url);
+                  }}
                 />
               );
             })
           : null}
       </div>
+      {photoView && photoViewSrc ? (
+        <div className="slider">
+          <span className="close" onClick={() => setPhotoView(false)}>
+            <FaWindowClose />
+          </span>
+          <img src={photoViewSrc} alt="" />
+        </div>
+      ) : null}
     </div>
   );
 }
